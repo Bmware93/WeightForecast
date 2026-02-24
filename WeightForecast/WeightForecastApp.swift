4beed31c8541d721b77b906cdb53b1d7c538137f
Benia Morgan <bmorganware@gmail.com>
2026-02-24 16:21:32 -0500

    Initial Commit

---
+++ WeightForecast/WeightForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeightForecastApp.swift
+//  WeightForecast
+//
+//  Created by Benia Morgan-Ware on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct WeightForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
