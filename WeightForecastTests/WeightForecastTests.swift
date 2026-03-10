489bf36c762d89ec02d04e526ccad15987c6f3f5
Benia Morgan <bmorganware@gmail.com>
2026-03-10 09:53:10 -0400

    made tests for converting lbs to kgs. Adding tests and units are being converted correctly.

---
+++ WeightForecastTests/WeightForecastTests.swift
@@ -0,0 +1,16 @@
+//
+//  WeightForecastTests.swift
+//  WeightForecastTests
+//
+//  Created by Benia Morgan-Ware on 3/9/26.
+//
+
+import Testing
+
+struct WeightForecastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
